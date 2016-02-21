51cbc1e56c1883f99233479565ba5f0e04cda423
Kenzie Liu <Kenzie Liu>
2016-02-21 16:47:59 +0800

    Add test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+L=['Bart','Lisa','Adam']
+for x in L:
+	print('hello', x)
+
+h=hex
+a=h(255)
+b=h(1000)
+print(a)
+print(b)
+
+import math
+
+def quadratic(a, b, c):
+	delta = (b ** 2) - 4 * a * c
+	delta_sqrt = math.sqrt(delta)
+	if delta > 0:
+		x = (-b + delta_sqrt) / (2 * a)
+		y = (-b - delta_sqrt) / (2 * a)
+		return (x, y)
+	elif delta < 0:
+		return None
+	else:
+		return -b / (2 * a)
+
+print(quadratic(2, 3, 1))
+print(quadratic(1, 3, -4))
+
